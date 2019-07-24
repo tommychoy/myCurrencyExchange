ac4f2d40d9852fc2fb2c02b0764216c0aaf2b41f
tommychoy <53161673+tommychoy@users.noreply.github.com>
2019-07-24 17:46:13 +0800

    Create get_token.sh

---
+++ get_token.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+function jsonval {
+    temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
+    echo ${temp##*|}
+}
+
+json=`curl -v -u tommychoy:09025417d@HP -X POST https://api.github.com/authorizations --data @tokenp.json`
+prop='token'
+token=`jsonval`
+echo "The token created is: $(token)"
