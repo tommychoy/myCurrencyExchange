b6a1737d0e44dc4cf811268acb87005128b58b9f
tommychoy <53161673+tommychoy@users.noreply.github.com>
2019-07-25 11:34:17 +0800

    Update get_token.sh

--- get_token.sh
+++ get_token.sh
@@ -4,11 +4,13 @@ function jsonval {
     echo ${temp##*|}
 }
 
+echo `curl -v -u tommychoy:09025417d@HP https://api.github.com/authorizations/`
+
 #delete the token
 curl -v -u tommychoy:09025417d@HP -X DELETE https://api.github.com/authorizations/311672435
 
 json=`curl -v -u tommychoy:09025417d@HP -X POST https://api.github.com/authorizations --data @tokenp.json`
 echo $json
 prop='token'
-token=`jsonval | cut -d' ' -f 1`
+token=`jsonval | cut -d' ' -f 2`
 echo $token >> token.txt
